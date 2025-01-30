1bec335d276ce09f4fc1e368d245d5339a221884
RichaJaiswal56 <jaiswalricha035@gmail.com>
2025-01-30 21:11:26 +0530

    QR Scanner by Richa Jaiswal

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
-const inputBox = document.querySelector('.input-box');
-const search_btn = document.getElementById('search-btn');
-const weather_img = document.querySelector('.weather-img');
-const temp = document.querySelector('.temperature');
-const descript = document.querySelector('.description');
-const humid = document.getElementById('humidity');
-const windSpeed = document.getElementById('wind-speed');
-const location_not_found=document.querySelector('.location-not-found');
-const weather_body=document.querySelector('.weather-body');
+const file_input=document.querySelector("input");
+const img=document.querySelector("img");
+const video=document.querySelector("video");
+const qr_code_view=document.querySelector(".qr-code-view");
+const icons=document.querySelector(".icons");
+const qr_text=document.querySelector(".qr-text");
+const message=document.querySelector("p");
+const textarea=document.querySelector("textarea");
 
-async function weatherCheck(city) {
-    const api_key = "d7d19d60d210bafa7ddeb9d338559a2a";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-    const weather_data = await fetch(`${url}`).then(response =>
-        response.json());
+file_input.addEventListener("change", (e)=>{
+    let file=e.target.files[0];
+    if(!file) return;
+    fetchQRCodeResponse(file);
 
 
-        if(weather_data.cod==`404`){
-            location_not_found.style.display="flex";
-            weather_body.style.display="none";
-        //    console.log("error");
-            return;
-        }
-        else{
-            location_not_found.style.display="none";
-        }
-        
-        weather_body.style.display="flex";
+})
+function fetchQRCodeResponse(file){
+    let formData = new FormData();
+    // let formData;
+    // FormData=new formData;
+    formData.append("file",file);
+    fetch("https://api.qrserver.com/v1/read-qr-code/",{
+    method:"POST",
+    body: formData,
+    }).then((response)=>response.json())
+    .then((data) => {
+        textarea.innerText=" ";
+        let result=data[0].symbol[0].data;
+
+        if(!result) return(message.innerText="Couldn't scan QR");
     
-
-
-    // console.log(weather_data)
-
-
-    temp.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-    descript.innerHTML = `${weather_data.weather[0].description}`;
-    humid.innerHTML = `${weather_data.main.humidity}%`;
-    windSpeed.innerHTML = `${weather_data.wind.speed}Km/H`;
-
-
-    switch (weather_data.weather[0].main) {
-        case 'Clouds':
-            weather_img.src = "/images/cloud.png";
-            break;
-
-        case 'Rain':
-            weather_img.src = "/images/rain.png";
-            break;
-        case 'Mist':
-            weather_img.src = "/images/mist.png";
-            break;
-        case 'Clear':
-            weather_img.src = "/images/clear.png";
-            break;
-            case 'Snow':
-            weather_img.src = "/images/snow.png";
-            break;
-
+        qr_text.style.display="block";
+        textarea.innerText=result;
+        img.style.display="block";
+        img.src=URL.createObjectURL(file);
+        icons.style.display="none";
+        
+});
+}
+function copyQRtext(){
+    let text=textarea.textContent;
+    navigator.clipboard.writeText(text);
+}
+function closeQRscanner(){
+    message.innerText="Upload or Scan QR Code to Read";
+    icons.style.display="block";
+    img.style.display="none";
+    video.style.display="none";
+    qr_text.style.display="none";
+    textarea.innerText=" ";
+    file_input.value=" ";
+}
+function scanQRcode(){
+    let scanner= new Instascan.Scanner({video:video,captureImage: true});
+    if(scanner){
+        message.innerText="Loading Camera....." 
     }
 }
 
-search_btn.addEventListener('click', () => {
-    weatherCheck(inputBox.value);
-});
+// console.log(file_input);
